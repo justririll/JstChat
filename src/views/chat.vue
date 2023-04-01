b76d2ac18c3d5ff2351449a316ddca7cf32583d7
justririll <cate1233211@gmail.com>
2023-04-01 18:14:03 +0400

    removed tf

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -37,7 +37,7 @@
         // other:
         PersonalEmotes: {},
 
-        aprilFool: new Audio('https://sound-pack.net/download/Sound_08029.mp3'),
+        // aprilFool: new Audio('https://sound-pack.net/download/Sound_08029.mp3'),
 
         connectedToChat: false,
 
@@ -155,9 +155,9 @@
             payload.PersonalEmotes = this.PersonalEmotes[payload.source.nick]
           }
 
-          if (payload.source.nick == "rj_st" && payload.parameters.trim() == "123") {
-            this.aprilFool.play()
-          }
+          // if (payload.source.nick == "rj_st" && payload.parameters.trim() == "123") {
+          //   this.aprilFool.play()
+          // }
 
           this.Messages.push(payload)
         }
